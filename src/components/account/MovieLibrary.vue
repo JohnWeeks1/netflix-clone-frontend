383870751915f1bb5853c448c1bee39a5359829c
Jonathan Weeks <jonathanweeks007@gmail.com>
2020-09-16 11:53:29 +0100

    Removed isPending from function

--- src/components/account/MovieLibrary.vue
+++ src/components/account/MovieLibrary.vue
@@ -26,7 +26,6 @@ export default {
   },
   methods: {
      getAllMovies() {
-       this.isPending = true;
        axios.get('api/movies')
           .then(response => {
             this.movies = response.data.data;
